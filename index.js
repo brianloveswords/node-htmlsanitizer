01bea9dd647749442645e6933d4af7578fc83778
Brian J Brennan <brianloveswords@gmail.com>
2012-09-15 20:09:52 -0400

    Mostly done with MVP

---
+++ index.js
@@ -0,0 +1,51 @@
+var url = require('url');
+var http = require('http');
+
+var SANITIZATION_ENDPOINT = 'http://htmlsanitizer.org';
+
+function sanitize(stringOrOptions, callback) {
+  var options = stringOrOptions;
+  if (typeof stringOrOptions === 'string')
+    options = { text: stringOrOptions };
+  sanitize.request(options, callback)
+}
+sanitize.ENDPOINT = SANITIZATION_ENDPOINT;
+
+sanitize.request = function request(options, callback) {
+  var json = sanitize.prepareJson(options);
+  var endpoint = options.endpoint || sanitize.ENDPOINT;
+  var requestOptions = url.parse(endpoint);
+  requestOptions.headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': json.length
+  };
+  requestOptions.method = 'POST';
+  http.request(requestOptions, function (res) {
+    var fullResponse = '';
+    res.on('data', function (buf) { fullResponse += buf });
+    res.on('end', function () { callback(null, fullResponse) });
+  }).on('error', function (err) {
+    return callback(err);
+  }).write(json);
+};
+
+sanitize.setEndpoint = function setEndpoint(endpoint) {
+  return sanitize.ENDPOINT = endpoint;
+};
+
+sanitize.prepareJson = function prepareJson(options) {
+  var result = Object.keys(options).reduce(function (accum, key) {
+    accum[toUnderscore(key)] = options[key];
+    return accum;
+  }, {})
+  return JSON.stringify(result);
+};
+
+function toUnderscore(input) {
+  var expression = /([a-z])([A-Z])/g;
+  return input.replace(expression, function (_, lower, upper) {
+    return lower + '_' + upper.toLowerCase();
+  });
+}
+
+module.exports = sanitize
---
+++ tests/sanitize.test.js
@@ -0,0 +1,80 @@
+var test = require('tap').test;
+var sanitize = require('../');
+
+var endpoint = process.env['ENDPOINT'];
+if (endpoint)
+  sanitize.setEndpoint(endpoint);
+
+test('sanitize: given no html, should be the same', function (t) {
+  var str = 'what';
+  sanitize(str, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, str, 'should be the same');
+    t.end();
+  });
+});
+
+test('sanitize: given safe html, should be the same', function (t) {
+  var str = '<strong>what</strong>';
+  sanitize(str, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, str, 'should be the same');
+    t.end();
+  });
+});
+
+test('sanitize: given unsafe html, should be different', function (t) {
+  var input = '<script>what</script>';
+  var expect = '&lt;script&gt;what&lt;/script&gt;';
+  sanitize(input, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, expect, 'should get safe html');
+    t.end();
+  });
+});
+
+test('sanitize: given a tag list, should respect', function (t) {
+  var input = '<script>what</script>';
+  sanitize({ text: input, tags: ['script'] }, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, input, 'should get same thing back');
+    t.end();
+  });
+});
+
+test('sanitize: strip attrs by default', function (t) {
+  var input = '<strong title="awesome">what</strong>';
+  var expect = '<strong>what</strong>';
+  sanitize({ text: input }, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, expect, 'should get attr-less html');
+    t.end();
+  });
+});
+
+test('sanitize: allow attrs when specificed', function (t) {
+  var input = '<strong title="awesome">what</strong>';
+  sanitize({ text: input, attributes: { strong: ['title'] } }, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, input, 'should get attr-less html');
+    t.end();
+  });
+});
+
+test('sanitize: strips comments', function (t) {
+  var input = '<!-- sup -->';
+  sanitize({ text: input, attributes: { strong: ['title'] } }, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, '', 'should get commentless html');
+    t.end();
+  });
+});
+
+test('sanitize: strips comments unless specified not to', function (t) {
+  var input = '<!-- sup -->';
+  sanitize({ text: input, stripComments: false }, function (err, safe) {
+    t.notOk(err, 'should not have an error');
+    t.same(safe, input, 'should get commentfull html');
+    t.end();
+  });
+});
